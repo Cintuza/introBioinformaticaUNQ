9a2bcbeb2381a6fe8d177ad4b9e4809efc07dc2d
cintuza <cinbiblio@yahoo.com.ar>
2025-05-06 22:23:14 -0300

    cambios en enunciados

--- juegoDeLaVida/juegoExpresionGenica.py
+++ juegoDeLaVida/juegoExpresionGenica.py
@@ -54,7 +54,8 @@ class JuegoExpresionGenica:
 
     def acoplarseAlADN(self):
         respuesta = input(
-            "\n\033[1;35mIngresá A para ir directo a copiar una hebra de ADN, o ingresá B para deshacer la hélice de ADN: \033[0m\n")
+            "\n\033[1;35mIngresá A para ir directo a copiar una hebra de ADN, o ingresá B si crees que hay que "
+            "hacer un paso previo: \033[0m\n")
         while respuesta != "B":
             self.chequearSalida(respuesta)
             if respuesta == "A":
@@ -65,29 +66,29 @@ class JuegoExpresionGenica:
             else:
                 respuesta = input(
                     "Lo siento, no existe la opción indicada! Ingresá A para ir directo a copiar una hebra de ADN, "
-                    "o ingresá B para deshacer la hélice de ADN\n")
+                    "o ingresá B para hacer algún paso previo\n")
         self.encontrarGen()
 
 
     def encontrarGen(self):
         self.actualizarContadorConRespuestaCorrecta()
         respuesta = input(
-            "\nBien! Ahora que la hélice está extendida, debes encontrar dentro de la hebra molde del ADN el gen que "
-            "te dará la información necesaria para la proteína que querés sintetizar. ¿Qué hacés a continuación? "
-            "\033[1;35mIngresá A para buscar la región promotora que da inicio al gen que estás buscando, o ingresa B para "
-            "intentar plegarte en cualquier lugar de la cadena de ADN (hoy te sentís con suerte!) \033[0m\n")
+            "\nBien! Ahora que la hélice está extendida, debes encontrar el gen dentro de la cadena de ADN que "
+            "te dará la información necesaria para la proteína que querés sintetizar. ¿Por dónde empezar? "
+            "\033[1;35mIngresá A para buscar la región promotora, o ingresa B para buscar desde el inicio "
+            "de la cadena de ADN.\033[0m\n")
         while respuesta != "A":
             self.chequearSalida(respuesta)
             if respuesta == "B":
                 self.actualizarContadorConRespuestaIncorrecta()
                 respuesta = input(
                     "Parece que la suerte no logrará la expresión génica! Ingresá A para buscar la región promotora que "
-                    "da inicio al gen que estás buscando, o ingresa B para intentar plegarte en cualquier lugar de la "
+                    "da inicio al gen que estás buscando, o ingresa B para buscar desde el inicio de la "
                     "cadena de ADN \n")
             else:
                 respuesta = input(
                     "Lo siento, no existe la opción indicada! Ingresá A para buscar la región promotora que da inicio "
-                    "al gen que estás buscando, o ingresa B para intentar plegarte en cualquier lugar de la cadena de ADN\n")
+                    "al gen que estás buscando, o ingresa B para buscar desde el inicio de la cadena de ADN\n")
         self.corroborarCorrespondenciaNucleotidos()
 
 
@@ -116,24 +117,21 @@ class JuegoExpresionGenica:
     def iniciarTranscripcion(self):
         self.actualizarContadorConRespuestaCorrecta()
         respuesta = input(
-            "\nExcelente! Los nucleótidos timina de ADN deben ser sustituídos por nucleótidos uracilo de ARN. Entonces, "
-            "sabes cómo transcribir los nucléotidos, y encontraste la región promotora para empezar la transcripción. "
-            "\033[1;35mUna vez allí: A, decidis llevar vos misma la información del gen hasta la próxima etapa (no confiás en nadie "
-            "más para este trabajo!); o B, sintentizás una cadena de ARN mensajero y transcribís allí la información "
-            "(que viva el trabajo en equipo!) \033[0m\n")
+            "\nExcelente! Los nucleótidos timina de ADN deben ser sustituídos por nucleótidos uracilo de ARN. ¿Dónde "
+            "transcribís la información para que llegue al próximo paso? "
+            "\033[1;35mEscribí A si decidis si transcribís la información en un ARN de traducción; o B, si "
+            "transcribís la información en un ARN mensajero\033[0m\n")
         while respuesta != "B":
             self.chequearSalida(respuesta)
             if respuesta == "A":
                 self.actualizarContadorConRespuestaIncorrecta()
                 respuesta = input(
-                    "Parece que el intento de guardar vos misma los nucleótidos no funciona! Intentá de nuevo, ¿querés "
-                    "llevar vos misma la información del gen hasta la próxima etapa (opción A) o preferís sintentizar una "
-                    "cadena de ARN mensajero y transcribir allí la información (opción B)?\n")
+                    "¡Parece que no existe lo que buscás! ¿Querés buscar un ARN de traducción (opción A) o preferís "
+                    "sintentizar una cadena de ARN mensajero y transcribir allí la información (opción B)?\n")
             else:
                 respuesta = input(
-                    "Lo siento, no existe la opción indicada! ¿Querés llevar vos misma la información del gen hasta "
-                    "la próxima etapa (opción A) o preferís sintentizar una cadena de ARN mensajero y transcribir allí "
-                    "la información (opción B)? \n")
+                    "Lo siento, no existe la opción indicada! ¿Querés buscar un ARN de traducción (opción A) o "
+                    "preferís sintentizar una cadena de ARN mensajero y transcribir allí la información (opción B)? \n")
 
         print(
             "\nLa transcripción ha sido realizada con éxito: la cadena de ARN mensajero está lista, tu trabajo como "
@@ -144,26 +142,27 @@ class JuegoExpresionGenica:
     def encontrarRibosoma(self):
         self.actualizarContadorConRespuestaCorrecta()
         respuesta = input(
-            "\nComo ARN mensajero, debes encontrar un ribosoma, que construirá la proteína en base a la información "
-            "que llevás. \033[1;35mEscribí A para salir del núcleo celular, o B para buscar un ribosoma dentro del núcleo. \033[0m\n")
+            "\nComo ARN mensajero, salís del núcleo para buscar la organela que construirá la proteína en base "
+            "a la información que llevás. \033[1;35mEscribí A si crees que debés buscar un ribosoma para realizar "
+            "esta tarea, o B si crees que debés buscar una mitocontria.\033[0m\n")
         while respuesta != "A":
             self.chequearSalida(respuesta)
             if respuesta == "B":
                 self.actualizarContadorConRespuestaIncorrecta()
                 respuesta = input(
-                    "¡Encontrar un ribosoma en el núcleo está difícil! Escribí A para salir del núcleo celular, o B "
-                    "para seguir busando un ribosoma dentro del núcleo. \n")
+                    "¡La mitocondria no parece preparada para esta tarea! Escribí A para buscar un ribosoma, o B "
+                    "para seguir probando con una mitocondria.\n")
             else:
                 respuesta = input(
-                    "Lo siento, no existe la opción indicada! Escribí A para salir del núcleo celular, o B para buscar "
-                    "un ribosoma dentro del núcleo.  \n")
+                    "Lo siento, no existe la opción indicada! Escribí A para buscar un ribosoma, o B "
+                    "para seguir probando con una mitocondria.\n")
         self.ingresarAlRibosoma()
 
 
     def ingresarAlRibosoma(self):
         self.actualizarContadorConRespuestaCorrecta()
         respuesta = input(
-            "\nSaliste del núcleo y encontraste un ribosoma! Allí transcurrirá la traducción y síntesis de la proteína. "
+            "\nEn el ribosoma transcurrirá la traducción y síntesis de la proteína. "
             "El ribosoma posee una subunidad grande y una subunidad pequeña entre las cuales irás avanzando para que se "
             "pueda leer toda tu secuencia de a tres nucleótidos a la vez (lo que se denomina codón). \033[1;35mPero momento, ¿cómo "
             "sabrá en qué codón de tu secuencia debe empezar a traducir? Escribí A para que el ribosoma arranque al "
@@ -186,21 +185,20 @@ class JuegoExpresionGenica:
     def buscarARNDeTraduccion(self):
         self.actualizarContadorConRespuestaCorrecta()
         respuesta = input(
-            "\n¡Así es! Existe un codón de inicio, generalmente el primer triplete de nucleótidos AUG, que indica dónde "
-            "iniciará la secuencia peptídica; al ser AUG, el primer aminoácido especificado es la metionina. Pero "
+            "\n¡Así es! Existe un codón de inicio que indica dónde iniciará la secuencia peptídica. Pero "
             "alguien debe unirse a tus codones para interpretarlos y traer el aminoácido correspondiente a cada uno "
-            "(lo que denominamos traducción). \033[1;35m¿Quién hará esta tarea? Indica A si crees que será otra organela, o B "
-            "si crees que será el ARN de transferencia: \033[0m\n")
+            "(lo que denominamos traducción). \033[1;35m¿Quién hará esta tarea? Indica A si crees que será otro ARN mensajero, o B "
+            "si crees que será un ARN de transferencia: \033[0m\n")
         while respuesta != "B":
             self.chequearSalida(respuesta)
             if respuesta == "A":
                 self.actualizarContadorConRespuestaIncorrecta()
                 respuesta = input(
-                    "¡Ninguna otra organela parece querer hacer el trabajo! Indica A si quieres seguir buscando otra "
-                    "organela, o B si crees que será el ARN de transferencia:  \n")
+                    "¡Ninguna otra organela parece querer hacer el trabajo! Indica A si quieres seguir buscando otro "
+                    "ARN mensajero, o B si crees que será el ARN de transferencia:  \n")
             else:
                 respuesta = input(
-                    "Lo siento, no existe la opción indicada! Indica A si crees que será otra organela, o B si crees "
+                    "Lo siento, no existe la opción indicada! Indica A si crees que será otro ARN mensajero, o B si crees "
                     "que será el ARN de transferencia:  \n")
         self.finalizarTraduccion()
 
@@ -213,19 +211,19 @@ class JuegoExpresionGenica:
             "ribosoma atrae a cada ARN de transferencia correspondiente y lo une a tus codones, la subunidad más grande "
             "separa el aminoácido y comienza a armar la cadena protéica. Una vez que el ARN de transferencia dejó su "
             "aminoácido, se libera y deja lugar al ARN de transferencia siguiente, mientras tu cadena se mueve a su vez "
-            "al siguiente codón. \033[1;35m¿Cuándo termina este proceso? Ingresá A si crees que el ribosoma da la señal de fin, "
-            "o ingresá B si crees que vos como cadena de ARN mensajero das la señal de fin: \033[0m\n")
+            "al siguiente codón. \033[1;35m¿Cuándo termina este proceso? Ingresá A si crees que el ARN de transferencia "
+            "da la señal de fin, o ingresá B si crees que vos como cadena de ARN mensajero das la señal de fin: \033[0m\n")
         while respuesta != "B":
             self.chequearSalida(respuesta)
             if respuesta == "A":
                 self.actualizarContadorConRespuestaIncorrecta()
                 respuesta = input(
                     "¡Parece que el ribosoma no encuentra forma de indicar el fin de la traducción! Ingresá A para que "
-                    "el ribosoma siga intentando dar la señal de fin, o ingresá B si crees que vos como cadena de "
+                    "el ARN de transferencia siga intentando dar la señal de fin, o ingresá B si crees que vos como cadena de "
                     "ARN mensajero debés dar la señal: \n")
             else:
                 respuesta = input(
-                    "Lo siento, no existe la opción indicada! Ingresá A si crees que el ribosoma da la señal de fin, "
+                    "Lo siento, no existe la opción indicada! Ingresá A si crees que el ARN de transferencia da la señal de fin, "
                     "o ingresá B si crees que vos como cadena de ARN mensajero das la señal de fin:  \n")
         self.actualizarContadorConRespuestaCorrecta()
         proteina2.imprimirProteinaFinal()
